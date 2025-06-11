b22756cf0e1fc71cd08b57c23eb50c17ab05d377
Myruziyev <myruziyev@gmail.com>
2025-06-11 18:07:29 +0500

    dfasf

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-  
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, illo!
+      </p>
+      <h3>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, quae.
+        Quasi perspiciatis eligendi doloremque temporibus iusto id accusamus
+        ipsa pariatur, nam deserunt cumque eum ratione asperiores laboriosam
+        eius soluta. Suscipit? Veniam nesciunt consequuntur perferendis labore
+        quia et repudiandae facilis delectus unde sunt aperiam vel qui, impedit
+        reprehenderit repellendus expedita natus neque sed recusandae eum
+        laboriosam. Fugit illo dolore molestias dignissimos. Molestiae at enim
+        quam recusandae, velit magni corrupti est reiciendis ipsa rem doloribus
+        quibusdam voluptatem tenetur veniam id, incidunt delectus, dignissimos
+        laudantium libero doloremque aperiam. Minus quo eius laborum
+        necessitatibus!
+      </h3>
+      <h4>
+        lorm ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatibus, cumque, quod, veritatis, voluptates doloribus
+      </h4>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
